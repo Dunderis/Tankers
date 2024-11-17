32842aa81280a2d30bd100ff3d9ebc6fa0082dc5
Simona <sima.simute1999@gmail.com>
2024-11-17 16:09:29 +0200

    player 2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //todo: explosion effect
+        //todo: damage enemy
+        Destroy(gameObject);
+    }
+}
