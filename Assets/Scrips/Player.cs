b362746e2e602285c44457f822ffd3a4c537b5fe
Simona <sima.simute1999@gmail.com>
2024-11-17 14:24:55 +0200

    Player 2

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool isPlayer1 = true;
+
     [Header("Movement")]
     public float speed = 10.0f;
 
+    [Header("Shooting")]
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireRate = 0.5f;
+
+
+
     void Start()
     {
-        
+        InvokeRepeating("Shoot", 0.0f, fireRate);
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, bulletSpawn.position, transform.rotation);
     }
 
     void Update()
     {
         var input = new Vector3();
-        input.x = Input.GetAxis("Horizontal");
-        input.z = Input.GetAxis("Vertical");
+        
+        if(isPlayer1)
+        {
+            input.x = Input.GetAxis("HorizontalKeys");
+            input.z = Input.GetAxis("VerticalKeys");
+        }
+        else
+        {
+            input.x = Input.GetAxis("HorizontalArrows");
+            input.z = Input.GetAxis("VerticalArrows");
+        }
+        
 
         transform.position += input * speed * Time.deltaTime;
 
